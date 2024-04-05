0d79815217e35b1759db00c2d0e728091e84529d
Joe <jakatsa5@gmail.com>
2024-04-05 20:23:48 +0300

    firestore updates

--- socialmedia/src/pages/create-posts/CreateForms.jsx
+++ socialmedia/src/pages/create-posts/CreateForms.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { auth } from "../../config/firebase";
 export const CreateForms = () => {
+  const [user] = useAuthState(auth);
   const schema = yup.object().shape({
     title: yup.string().required("Add Title"),
     description: yup.string().required("Add Description"),
@@ -14,8 +19,13 @@ export const CreateForms = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onCreatePost = (data) => {
-    console.log(data);
+  const postsRef = collection(db, "posts");
+  const onCreatePost = async (data) => {
+    await addDoc(postsRef, {
+      ...data,
+      username: user?.displayName,
+      userId: user?.uid,
+    });
   };
   return (
     <form onSubmit={handleSubmit(onCreatePost)}>
